6b4ba91740042d108ca0671a5a463a879b37111a
zhih <fufong79570@gmail.com>
2022-03-07 01:27:32 +0800

    Add member tag

--- config/member.js
+++ config/member.js
@@ -1,49 +1,184 @@
 module.exports.MEMBERS = {
-  nogi:{
-  "1": { "name": "秋元 真夏" },
-  "2": { "name": "生田 絵梨花" },
-  "3": { "name": "齋藤 飛鳥" },
-  "4": { "name": "高山 一実" },
-  "5": { "name": "樋口 日奈" },
-  "6": { "name": "星野 みなみ" },
-  "7": { "name": "松村 沙友理" },
-  "8": { "name": "和田 まあや" },
-  "9": { "name": "伊藤 純奈" },
-  "10": { "name": "北野 日奈子" },
-  "11": { "name": "新内 眞衣" },
-  "12": { "name": "鈴木 絢音" },
-  "13": { "name": "寺田 蘭世" },
-  "14": { "name": "堀 未央奈" },
-  "15": { "name": "山崎 怜奈" },
-  "16": { "name": "渡辺 みり愛" },
-  "17": { "name": "伊藤 理々杏" },
-  "18": { "name": "岩本 蓮加" },
-  "19": { "name": "梅澤 美波" },
-  "20": { "name": "大園 桃子" },
-  "21": { "name": "久保 史緒里" },
-  "22": { "name": "阪口 珠美" },
-  "23": { "name": "佐藤 楓" },
-  "24": { "name": "中村 麗乃" },
-  "25": { "name": "向井 葉月" },
-  "26": { "name": "山下 美月" },
-  "27": { "name": "吉田 綾乃ｸﾘｽﾃｨｰ" },
-  "28": { "name": "与田 祐希" },
-  "29": { "name": "遠藤 さくら" },
-  "30": { "name": "賀喜 遥香" },
-  "31": { "name": "掛橋 沙耶香" },
-  "32": { "name": "金川 紗耶" },
-  "33": { "name": "北川 悠理" },
-  "34": { "name": "黒見 明香" },
-  "35": { "name": "佐藤 璃果" },
-  "36": { "name": "柴田 柚菜" },
-  "37": { "name": "清宮 レイ" },
-  "38": { "name": "田村 真佑" },
-  "39": { "name": "筒井 あやめ" },
-  "40": { "name": "早川 聖来" },
-  "41": { "name": "林 瑠奈" },
-  "42": { "name": "松尾 美佑" },
-  "43": { "name": "矢久保 美緒" },
-  "44": { "name": "弓木 奈於" },
-  "45": { "name": "乃木坂46" }
-}
+  nogi: {
+    1: {
+      name: "秋元 真夏",
+      tag: "nogizaka-1",
+    },
+    2: {
+      name: "生田 絵梨花",
+      tag: "nogizaka-1",
+    },
+    3: {
+      name: "齋藤 飛鳥",
+      tag: "nogizaka-1",
+    },
+    4: {
+      name: "高山 一実",
+      tag: "nogizaka-1",
+    },
+    5: {
+      name: "樋口 日奈",
+      tag: "nogizaka-1",
+    },
+    6: {
+      name: "星野 みなみ",
+      tag: "nogizaka-1",
+    },
+    7: {
+      name: "松村 沙友理",
+      tag: "nogizaka-1",
+    },
+    8: {
+      name: "和田 まあや",
+      tag: "nogizaka-1",
+    },
+    9: {
+      name: "伊藤 純奈",
+      tag: "nogizaka-2",
+    },
+    10: {
+      name: "北野 日奈子",
+      tag: "nogizaka-2",
+    },
+    11: {
+      name: "新内 眞衣",
+      tag: "nogizaka-2",
+    },
+    12: {
+      name: "鈴木 絢音",
+      tag: "nogizaka-2",
+    },
+    13: {
+      name: "寺田 蘭世",
+      tag: "nogizaka-2",
+    },
+    14: {
+      name: "堀 未央奈",
+      tag: "nogizaka-2",
+    },
+    15: {
+      name: "山崎 怜奈",
+      tag: "nogizaka-2",
+    },
+    16: {
+      name: "渡辺 みり愛",
+      tag: "nogizaka-2",
+    },
+    17: {
+      name: "伊藤 理々杏",
+      tag: "nogizaka-3",
+    },
+    18: {
+      name: "岩本 蓮加",
+      tag: "nogizaka-3",
+    },
+    19: {
+      name: "梅澤 美波",
+      tag: "nogizaka-3",
+    },
+    20: {
+      name: "大園 桃子",
+      tag: "nogizaka-3",
+    },
+    21: {
+      name: "久保 史緒里",
+      tag: "nogizaka-3",
+    },
+    22: {
+      name: "阪口 珠美",
+      tag: "nogizaka-3",
+    },
+    23: {
+      name: "佐藤 楓",
+      tag: "nogizaka-3",
+    },
+    24: {
+      name: "中村 麗乃",
+      tag: "nogizaka-3",
+    },
+    25: {
+      name: "向井 葉月",
+      tag: "nogizaka-3",
+    },
+    26: {
+      name: "山下 美月",
+      tag: "nogizaka-3",
+    },
+    27: {
+      name: "吉田 綾乃ｸﾘｽﾃｨｰ",
+      tag: "nogizaka-3",
+    },
+    28: {
+      name: "与田 祐希",
+      tag: "nogizaka-3",
+    },
+    29: {
+      name: "遠藤 さくら",
+      tag: "nogizaka-4",
+    },
+    30: {
+      name: "賀喜 遥香",
+      tag: "nogizaka-4",
+    },
+    31: {
+      name: "掛橋 沙耶香",
+      tag: "nogizaka-4",
+    },
+    32: {
+      name: "金川 紗耶",
+      tag: "nogizaka-4",
+    },
+    33: {
+      name: "北川 悠理",
+      tag: "nogizaka-4",
+    },
+    34: {
+      name: "黒見 明香",
+      tag: "nogizaka-4",
+    },
+    35: {
+      name: "佐藤 璃果",
+      tag: "nogizaka-4",
+    },
+    36: {
+      name: "柴田 柚菜",
+      tag: "nogizaka-4",
+    },
+    37: {
+      name: "清宮 レイ",
+      tag: "nogizaka-4",
+    },
+    38: {
+      name: "田村 真佑",
+      tag: "nogizaka-4",
+    },
+    39: {
+      name: "筒井 あやめ",
+      tag: "nogizaka-4",
+    },
+    40: {
+      name: "早川 聖来",
+      tag: "nogizaka-4",
+    },
+    41: {
+      name: "林 瑠奈",
+      tag: "nogizaka-4",
+    },
+    42: {
+      name: "松尾 美佑",
+      tag: "nogizaka-4",
+    },
+    43: {
+      name: "矢久保 美緒",
+      tag: "nogizaka-4",
+    },
+    44: {
+      name: "弓木 奈於",
+      tag: "nogizaka-4",
+    },
+    45: {
+      name: "乃木坂46",
+      tag: "nogizaka",
+    },
+  },
 };
